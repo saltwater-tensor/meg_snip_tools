c8c3f5b1a27b2360e42de58b0abbf5e891a7d2b7
saltwater-tensor <75883359+saltwater-tensor@users.noreply.github.com>
2023-02-09 17:11:25 +0100

    Add files via upload

--- brainstorm_montage_creator.m
+++ brainstorm_montage_creator.m
@@ -1,5 +1,6 @@
 % List the bad channels here
-bad_chans = ['EEG008'];
+bad_chans = ['EEG008,EEG033,EEG034,EEG001,EEG003']; %THERE SHOULD BE NO SPACE AFTER OR BEFORE THE ','
+% bad_chans = ['EEG008']; %THERE SHOULD BE NO SPACE AFTER OR BEFORE THE ','
 % Based on the system the patient had run either abbot or medtronic
 device_name = 'medtronic';
 
@@ -14,13 +15,13 @@ end
 
 % Reformat the montage for Brainstorm
 Montages = struct();
-Montages.Name = 'BipolarLFPsauto';
 Montages.Type = 'text';
 Montages.Matrix = montage.tra;
 Montages.DispNames = montage.labelnew';
 Montages.ChanNames = montage.labelold';
 
 % Provide a name for your montage
-montage_name = 'Bipolar_LFPs';
+montage_name = 'S01_Bipolar_LFPs';
+Montages.Name = [montage_name, '_', device_name]; % This is the name shown in Brainstorm
 
 save(['montage_', montage_name, '_', device_name],'Montages')
